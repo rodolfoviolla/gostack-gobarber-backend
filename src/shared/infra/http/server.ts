4540a0e39dec4afda8c08f7c204eb80010ffd458
Rodolfo Della Violla <rodolfo.violla@gmail.com>
2020-08-20 15:48:20 -0300

    Correção erro CORS

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -21,7 +21,7 @@ app.use(
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
-    optionsSuccessStatus: 204,
+    optionsSuccessStatus: 200,
   }),
 );
 app.use(express.json());
